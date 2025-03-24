bad1c4da478af023eac8b441e3906e44c0d4dd9d
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-03-24 16:25:15 +0800

    Changes to script

--- az.sh
+++ az.sh
@@ -31,83 +31,13 @@ case "$ACTION" in
     kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.6.4/components.yaml
     kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
     kubectl apply --server-side=true --force-conflicts -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.21/releases/cnpg-1.21.3.yaml
-    ;;
-  install-cert-manager)
-    # Install cert-manager
     kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.1/cert-manager.yaml
-    echo "Cert-manager installed. Waiting for pods to be ready..."
-    kubectl wait --for=condition=Ready pod -l app=cert-manager -n cert-manager --timeout=60s
-    ;;
-  setup-ssl)
-    # Takes domain as parameter: ./az.sh setup-ssl example.com
-    DOMAIN=$2
-    if [ -z "$DOMAIN" ]; then
-      echo "Domain parameter required. Usage: $0 setup-ssl yourdomain.com"
-      exit 1
-    fi
-    
-    # Get the ingress IP address
-    IP=$(kubectl get service ingress-nginx-controller -n ingress-nginx -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-    echo "Ingress IP: $IP"
-    echo "Setting up SSL for domain: $DOMAIN"
-    echo "Please ensure your DNS A record for $DOMAIN points to $IP"
-    
-    # Create ClusterIssuer for Let's Encrypt
-    cat <<EOF | kubectl apply -f -
-apiVersion: cert-manager.io/v1
-kind: ClusterIssuer
-metadata:
-  name: letsencrypt-prod
-spec:
-  acme:
-    server: https://acme-v02.api.letsencrypt.org/directory
-    email: admin@${DOMAIN}
-    privateKeySecretRef:
-      name: letsencrypt-prod
-    solvers:
-    - http01:
-        ingress:
-          class: nginx
-EOF
-    
-    # Update the ingress to use TLS
-    cat <<EOF | kubectl apply -f -
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: odoo
-  annotations:
-    nginx.ingress.kubernetes.io/proxy-body-size: "50m"
-    nginx.ingress.kubernetes.io/proxy-read-timeout: "600"
-    nginx.ingress.kubernetes.io/proxy-send-timeout: "600"
-    nginx.ingress.kubernetes.io/ssl-redirect: "true"
-    cert-manager.io/cluster-issuer: "letsencrypt-prod"
-spec:
-  ingressClassName: nginx
-  tls:
-  - hosts:
-    - ${DOMAIN}
-    secretName: odoo-tls-cert
-  rules:
-  - host: ${DOMAIN}
-    http:
-      paths:
-      - path: /
-        pathType: Prefix
-        backend:
-          service:
-            name: odoo
-            port:
-              number: 80
-EOF
-
-    echo "SSL setup initiated. Certificate will be issued once DNS propagation is complete."
-    echo "Check status with: kubectl get certificate"
     ;;
   uninstall)
     kubectl delete -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.6.4/components.yaml
     kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
     kubectl delete -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.21/releases/cnpg-1.21.3.yaml
+    kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.1/cert-manager.yaml
     ;;
   get-ip)
     echo "Getting Ingress IP..."
